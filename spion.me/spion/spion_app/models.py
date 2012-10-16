652bb653ee5ff6d84e273df7f3c95571a71d290f
Eric Schrijver <eric@ericschrijver.nl>
2012-10-16 11:50:01 +0200

    Some pseudo-code to remember Seda’s comments on the structure of the models

--- spion.me/spion/spion_app/models.py
+++ spion.me/spion/spion_app/models.py
@@ -7,6 +7,11 @@ from django.db import models
 
 from django.contrib import admin
 
+# class WorkPackage
+#     description
+#     researchers (foreignfield profile)
+
+# admin.site.register(WorkPackage)
 
 class Resource(models.Model):
     path = models.FileField(upload_to='resources/%Y/%m/%d')
@@ -14,15 +19,26 @@ class Resource(models.Model):
 admin.site.register(Resource)
 
 class Publication(models.Model):
+    # title
+    # category = conference / journal / book / delivrable
+    # if delivrable:
+    #    models.ForeignKey('WorkPackage')
+    #    number
     published = models.IntegerField() # Assumed that year of publication was enough
     publisher = models.CharField(max_length=512)
     url = models.URLField()
     author = models.ForeignKey('UserProfile')
+    # There can be more than 1 user.
+    # Also: some of these authors are not necessarily Users
     
 admin.site.register(Publication)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
+    # group
+    # department
+    # university
+    # photo
     bio = models.TextField()
     
 admin.site.register(UserProfile)
