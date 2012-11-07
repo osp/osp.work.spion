878beb0f8ad8ed487d840e2ec02ee4f43267be93
Eric Schrijver <eric@ericschrijver.nl>
2012-11-08 00:42:19 +0100

    Some updates in anticipation of the conference.
    Now no longer uses the json, btw, just edit the html directly.

--- poster/live-grid/app-poster-final.py
+++ poster/live-grid/app-poster-final.py
@@ -3,27 +3,18 @@
 
 import codecs
 import json
-from flask import Flask, jsonify, render_template, request, g
+import os
+
+from flask import Flask, render_template, request, g
 app = Flask(__name__)
 
-@app.before_request
-def before_request():
-    """Read the json each time the page is displayed"""
-    f = codecs.open('widgets.json', 'r', 'utf-8')
-    g.widgets = json.loads(f.read())
-    f.close()
 
 @app.route('/')
 def index():
-    return render_template('index-overlap.html', widgets=g.widgets)
-
-@app.route('/store', methods = ['POST'])
-def store():
-    """foo = json.loads(request.data)
-    w = codecs.open('widgets.json', 'w', 'utf-8')
-    w.write(json.dumps(foo, indent=2, ensure_ascii=False))
-    w.close()"""
-    return "Not saved"
+    f = codecs.open('index.html', 'r', 'utf-8')
+    res = f.read()
+    f.close()
+    return res
 
 if __name__ == "__main__":
     app.run(debug=True)
