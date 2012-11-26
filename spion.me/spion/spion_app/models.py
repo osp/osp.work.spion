1c5af50e3aeca8c42f346b07d17eb5eb35cd8542
Pierre Marchand <pierre@oep-h.com>
2012-11-26 11:00:03 +0100

    revert to migration plan

--- spion.me/spion/spion_app/models.py
+++ spion.me/spion/spion_app/models.py
@@ -95,8 +95,6 @@ class Partner(models.Model):
 
         
 class SpionProject(models.Model):
-    description = models.TextField()
-    long_description = models.TextField()
     title = models.CharField(max_length=512, default="box title")
     description = models.TextField()
     
